d174d062f299dcd471392150423228307903c005
LER-Programming <github.lakeeffectrobotics@gmail.com>
2026-02-05 21:34:53 -0500

    change auto position for hub to use own alliance color (is 180 degrees off now)

--- src/main/java/frc/robot/AutoPositionSuppliers.java
+++ src/main/java/frc/robot/AutoPositionSuppliers.java
@@ -9,14 +9,14 @@ public class AutoPositionSuppliers
 {
     private Pose pose;
 
-    public AutoPositionSuppliers(Pose pose) // TODO: require team color
+    public AutoPositionSuppliers(Pose pose)
     {
         this.pose = pose;
     }
 
     public DoubleSupplier hubAngleSupplier = () -> {
         Pose2d robotPosition = pose.getRobotPose();
-        return Math.atan2(robotPosition.getY() - 4.1, robotPosition.getX() - 11.9);
+        return Math.atan2(robotPosition.getY() - 4.1, robotPosition.getX() - 4.6);
     };
 
 
