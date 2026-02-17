f8363a03a5f8e3b93534abdccb6ca0f1a64b2788
Adam G <adamgavey@gmail.com>
2026-02-16 19:16:43 -0500

    Simplified angle calculations, made a case for pure leftward motion, and added deltaX,Y measurements to SmartDashboard.

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -26,7 +26,7 @@ public class Camera extends SubsystemBase {
     public static double lastPoseXMeasured = 0.0;
     public static double lastPoseYMeasured = 0.0;
     public static double deltaPoseXMeasured = 0.0;
-    public static double detlaPoseYMeasured = 0.0;
+    public static double deltaPoseYMeasured = 0.0;
     public static double measuredAngle = 0.0;
 
     private Drivetrain drivetrain;
@@ -56,35 +56,32 @@ public class Camera extends SubsystemBase {
         nowPoseXMeasured = botpose[0];
         nowPoseYMeasured = botpose[1];
 
-        //Calculates measured angles in terms of cases (depending on what quadrant the robot is heading towards)
-        if (Math.abs(nowPoseXMeasured - lastPoseXMeasured) > 0.02 && Math.abs(nowPoseYMeasured - lastPoseYMeasured) > 0.02){
-            if (nowPoseXMeasured >=0 && nowPoseYMeasured >=0){
-                measuredAngle = Math.atan2(nowPoseXMeasured - lastPoseXMeasured, nowPoseYMeasured - lastPoseYMeasured);
-            } else if (nowPoseXMeasured <0 && nowPoseYMeasured >=0){
-                measuredAngle = Math.atan2(nowPoseXMeasured - lastPoseXMeasured, Math.abs(nowPoseYMeasured - lastPoseYMeasured)) + (3.1415926/2);
-            } else if (nowPoseXMeasured <0 && nowPoseYMeasured <0){
-                measuredAngle = Math.atan2(Math.abs(nowPoseXMeasured - lastPoseXMeasured), Math.abs(nowPoseYMeasured - lastPoseYMeasured)) + 3.1415926;
-            } else if (nowPoseXMeasured >=0 && nowPoseYMeasured <0){
-                measuredAngle = Math.atan2(Math.abs(nowPoseXMeasured - lastPoseXMeasured), nowPoseYMeasured - lastPoseYMeasured) + ((3*3.1415926)/2);
-            }
-        }
-        
+        deltaPoseXMeasured = nowPoseXMeasured - lastPoseXMeasured;//Calculates the change in X position since the last measurement. This is used to determine if the robot has moved enough to update the angle measurement.
+        deltaPoseYMeasured = nowPoseYMeasured - lastPoseYMeasured;//Calculates the  change in Y position since the last measurement. This is used to determine if the robot has moved enough to update the angle measurement.
 
-        //Comment the following 5 lines out when we get drive train
-        SmartDashboard.putNumber("measuredAngle", measuredAngle*(180/3.1415926));
-        SmartDashboard.putNumber("nowPoseXMeasured", nowPoseXMeasured);
-        SmartDashboard.putNumber("nowPoseYMeasured", nowPoseYMeasured);
-        SmartDashboard.putNumber("lastPoseXMeasured", lastPoseXMeasured);
-        SmartDashboard.putNumber("lastPoseYMeasured", lastPoseYMeasured);
+        //Calculates the angle of motion counterclockwise of the field-relative X axis. 
+        if (Math.abs(deltaPoseXMeasured) > 0.02 || Math.abs(deltaPoseYMeasured) > 0.02) {//Requires at least 2 cm of movement in either X or Y directions to update the angle. This is to prevent noise from the camera from causing erratic angle measurement.
+            measuredAngle = Math.atan2(deltaPoseYMeasured, deltaPoseXMeasured); //Math.atan2 receives arguments in the order (y,x). It automatically provides the correct angle for all four quadrants. Output is in radians CCW of X axis.
+            if (deltaPoseXMeasured < 0 && deltaPoseYMeasured == 0) { //If the robot is moving directly to the left, atan2 will return 180 degrees, but we want it to be -180 degrees to be consistent with the rest of our angle measurements. This corrects for that.
+                measuredAngle = -3.14;
+            }
+        //Data output to SmartDashboard for troubleshooting. Possibly comment the following lines out when we get the drive train back.
+            SmartDashboard.putNumber("measuredAngle", measuredAngle*(180/3.14)); //Converts the angle from radians to degrees for easier understanding. The camera provides the angle in radians, but degrees are more intuitive for most people.
+            SmartDashboard.putNumber("nowPoseXMeasured", nowPoseXMeasured);
+            SmartDashboard.putNumber("nowPoseYMeasured", nowPoseYMeasured);
+            SmartDashboard.putNumber("lastPoseXMeasured", lastPoseXMeasured);
+            SmartDashboard.putNumber("lastPoseYMeasured", lastPoseYMeasured);
+            SmartDashboard.putNumber("deltaPoseXMeasured", nowPoseXMeasured - lastPoseXMeasured);//Calculates the change in X position since the last measurement. This is used to determine if the robot has moved enough to update the angle measurement.
+            SmartDashboard.putNumber("deltaPoseYMeasured", nowPoseYMeasured - lastPoseYMeasured);//Calculates the change in Y position since the last measurement. This is used to determine if the robot has moved enough to update the angle measurement.
         
-        lastPoseXMeasured = nowPoseXMeasured;
-        lastPoseYMeasured = nowPoseYMeasured;
+            lastPoseXMeasured = nowPoseXMeasured;
+            lastPoseYMeasured = nowPoseYMeasured;
+        }
 
         //height = table.getEntry("height").getInteger(0);
         //SmartDashboard.putNumber("camera height", height);
         //lock = table.getEntry("lock").getBoolean(true);
         //SmartDashboard.putBoolean("has lock", lock);
-
     }
 
     public double getTx() {
