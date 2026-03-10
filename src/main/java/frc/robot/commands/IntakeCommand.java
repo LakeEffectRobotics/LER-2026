71fe23dbae03fefc428fef4662932453ea94f1fc
p-ifka <watercereal11@gmail.com>
2026-03-09 20:59:43 -0400

    change intakecommand to extend and retract intake based on controller trigger value

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -2,22 +2,44 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import java.util.function.DoubleSupplier;
 
 public class IntakeCommand extends Command{
-    Intake intake;
-    boolean intakeEnabled;
+    private static final long TIMEOUT_TIME = 60;
+    
+    private Intake intake;
+    private boolean intakeEnabled;
+    private long timer;
 
-    public IntakeCommand(Intake intake, boolean intakeEnabled){
+    public IntakeCommand(Intake intake, DoubleSupplier triggerSupplier){
         this.intake = intake;
+	this.triggerSupplier = triggerSupplier;
         this.intakeEnabled = intakeEnabled;
     }
 
     @Override
     public void execute(){
-        if(intakeEnabled == true){
-            intake.StartIntake();
-        } else {
-            intake.StopIntake();
-        }
+	intake.start();
+	intake.extend();
     }
+
+    @Override
+    public boolean isFinished()
+    {
+	if(triggerSupplier.getAsDouble() < 0.1) {
+	    if(timer >= TIMEOUT_TIME) {
+		return true;
+	    }
+	    timer++;
+	}
+	return false;
+    }
+
+    @Override
+    public void end(boolean isInterrupted)
+    {
+	intake.stop();
+	intake.retract();
+    }
+	
 }
