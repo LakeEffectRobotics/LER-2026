d1e24927b03272c5ad661253d32d2310520f4823
p-ifka <watercereal11@gmail.com>
2026-03-11 09:28:00 -0400

    add intake command for auto

---
+++ src/main/java/frc/robot/commands/auto/AutoIntakeCommand.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+
+/**
+ * IntakeCommand that enabled/disables the intake and ends, for auto
+ **/
+public class AutoIntakeCommand extends Command
+{
+    private Intake intake;
+    private boolean enable;
+
+    /** true if command was contructed with DoubleSuppliers for the target x and y **/
+
+    
+    public AutoIntakeCommand(Intake intake, boolean enable)
+    {
+	addRequirements(intake);
+	this.intake = intake;
+	this.enable = enable;
+    }
+
+    @Override
+    public void initialize()
+    {
+	if(enable) {
+	    intake.extend();
+	    intake.start();
+	} else {
+	    intake.stop();
+	}
+    }
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public boolean isFinished()
+    {
+	return true;
+    }
+    
+    @Override
+    public void end(boolean isInterrupted) { }
+}
